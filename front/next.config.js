38d6be904b5e49f7caa31e5678862a855c2daf75
Simone Alves <139272427+SimoneAlvess@users.noreply.github.com>
2024-01-04 22:34:22 -0300

    api hospedagem

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-  serverOptions: {
-    http: true,
-  },
-};
+}
+
+module.exports = nextConfig
